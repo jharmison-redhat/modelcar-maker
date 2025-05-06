b8cbff09e0c43bff3fc0e1b1b6f26564e313d8ef
James Harmison <jharmison@redhat.com>
2025-05-05 22:24:24 -0400

    Initial commit

---
+++ build.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e
+
+cd "$(dirname "$(realpath "$0")")"
+
+mapfile models <models.txt
+
+common_args=()
+
+if [ -n "$HF_TOKEN" ]; then
+    common_args+=(--secret id=hf-token,env=HF_TOKEN)
+elif [ -e .hf-token ]; then
+    common_args+=(--secret "id=hf-token,src=$(pwd)/.hf-token,type=file")
+elif [ -e ~/.cache/huggingface/token ]; then
+    common_args+=(--secret id=hf-token,src=~/.cache/huggingface/token,type=file)
+fi
+
+MODELCAR_REGISTRY="${MODELCAR_REGISTRY:-quay.io}"
+MODELCAR_REPO="${MODELCAR_REPO:-jharmison/models}"
+
+function nologin {
+    echo "Log in to ${MODELCAR_REGISTRY} or export REGISTRY_AUTH_FILE to point to a configuration where you are logged in." >&2
+    exit 1
+}
+if ! podman login --get-login "${MODELCAR_REGISTRY}" >/dev/null 2>&1; then
+    nologin
+fi
+
+for model in "${models[@]}"; do
+    tag="$(echo "$model" | sed -e 's/\//--/g' -e 's/./_/g' -e 's/.*/\L&/')-modelcar"
+    image="${MODELCAR_REGISTRY}/${MODELCAR_REPO}:${tag}"
+    podman build . --pull=newer "${common_args[@]}" --build-arg=NAME="${tag}" --build-arg=MODEL=$model -t "$image"
+    podman push "$image" || nologin
+done
