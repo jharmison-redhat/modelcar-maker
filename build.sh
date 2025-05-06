ba04baa918248eede97d5068901095fb56a5a36f
James Harmison <jharmison@redhat.com>
2025-05-05 22:58:15 -0400

    fix: handle branching/tokens better

--- build.sh
+++ build.sh
@@ -12,8 +12,8 @@ if [ -n "$HF_TOKEN" ]; then
     common_args+=(--secret id=hf-token,env=HF_TOKEN)
 elif [ -e .hf-token ]; then
     common_args+=(--secret "id=hf-token,src=$(pwd)/.hf-token,type=file")
-elif [ -e ~/.cache/huggingface/token ]; then
-    common_args+=(--secret id=hf-token,src=~/.cache/huggingface/token,type=file)
+elif [ -e "$HOME/.cache/huggingface/token" ]; then
+    common_args+=(--secret "id=hf-token,src=$HOME/.cache/huggingface/token,type=file")
 fi
 
 MODELCAR_REGISTRY="${MODELCAR_REGISTRY:-quay.io}"
@@ -23,15 +23,21 @@ function nologin {
     echo "Log in to ${MODELCAR_REGISTRY} or export REGISTRY_AUTH_FILE to point to a configuration where you are logged in." >&2
     exit 1
 }
+function branch {
+    git branch 2>/dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'
+}
+
 if ! podman login --get-login "${MODELCAR_REGISTRY}" >/dev/null 2>&1; then
     nologin
 fi
+
+common_args+=(--build-arg=REF=$(branch))
 
 for model in "${models[@]}"; do
     model_args=("--build-arg=MODEL=${model}")
     tag="$(echo "$model" | sed -e 's/\//--/g' -e 's/./_/g' -e 's/.*/\L&/')-modelcar"
     image="${MODELCAR_REGISTRY}/${MODELCAR_REPO}:${tag}"
-    model_args+=("--build-args=NAME=${tag}")
+    model_args+=("--build-arg=NAME=${tag}")
     podman build . "${common_args[@]}" "${model_args[@]}" -t "$image"
     podman push "$image" || nologin
 done
