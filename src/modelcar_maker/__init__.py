346f8c5ec32db06d63f8a02a86b3b867fcfaad3e
James Harmison <jharmison@redhat.com>
2025-06-18 10:24:06 -0400

    feat: enable process to handle more complex workflows

--- src/modelcar_maker/__init__.py
+++ src/modelcar_maker/__init__.py
@@ -1,13 +1,29 @@
 from pathlib import Path
 
 from .download import hf_download
-from .image import build
-from .image import push
+from .image import do_build
+from .image import do_image_rm
+from .image import do_push
+from .image import image_exists
 from .image import render
+from .util import settings
 
 
-def process(model: str, image_repo: str, authfile: Path | None = None) -> None:
+def process(
+    model: str,
+    image_repo: str = f"{settings.image.registry}/{settings.image.repository}",
+    authfile: Path | None = settings.image.get("authfile"),
+    push: bool = settings.image.push,
+    cleanup: bool = settings.image.cleanup,
+    skip_if_exists: bool = settings.image.skip_if_exists,
+) -> None:
+    if skip_if_exists:
+        if image_exists(model, image_repo):
+            return None
     download_dir = hf_download(model)
     render(model, download_dir)
-    build(model, image_repo, download_dir)
-    push(model, image_repo, authfile)
+    do_build(model, image_repo, download_dir)
+    if push:
+        do_push(model, image_repo, authfile)
+    if cleanup:
+        do_image_rm(model, image_repo)
